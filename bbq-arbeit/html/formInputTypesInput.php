a14e7a6a222b2973f261194b5675e8a2dc46f79e
pytherik <erikberndt@gmx.net>
2023-04-02 19:31:57 +0200

    add appleKeySymbolsHTML.md

--- bbq-arbeit/html/formInputTypesInput.php
+++ bbq-arbeit/html/formInputTypesInput.php
@@ -2,8 +2,8 @@
 <html lang="de">
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta name="viewport" content="width=device-width, user-scalable=no,
+  initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=">
   <link rel="stylesheet" href="../public/css/root.css">
   <link rel="stylesheet" href="../public/css/style.css">
@@ -11,51 +11,70 @@
 </head>
 <body>
 <div class="container">
-  <div class="form-container mt3">
+  <div class="form-container mt2">
     <form action="formInputTypesOutput.php" method="post">
       <div class="input-container mb1">
         <label for="vorname">Vorname</label><br>
-        <input class="txt tra-li o-off" type="text" name="vorname" id="vorname" autofocus><br>
+        <input class="txt tra-li o-off" type="text"
+               name="vorname" id="vorname" size="40" autofocus><br>
       </div>
       <div class="input-container mb1">
         <label for="nachname">Nachname</label><br>
-        <input class="txt tra-li o-off" type="text" name="nachname" id="nachname"><br><br>
+        <input class="txt tra-li o-off" type="text"
+               name="nachname" id="nachname" size="40"><br><br>
       </div>
       <div class="input-container mb2">
         <label for="email">Email</label><br>
-        <input class="txt tra-li o-off" type="email" name="email" id="email"><br><br>
+        <input class="txt tra-li o-off" type="email"
+               name="email" id="email" size="40"><br>
       </div>
       <div class="input-container">
         <span>Geschlecht:</span><br>
         <label for="w">w</label>
-        <input class="mt0" type="radio" name="geschlecht" id="w" value="weiblich" checked>
+        <input class="mt0" type="radio" id="w"
+               name="geschlecht" value="weiblich" checked>
         <label for="m">m</label>
-        <input type="radio" name="geschlecht" id="m" value="männlich">
+        <input type="radio" id="m"
+               name="geschlecht" value="männlich">
         <label for="d">d</label>
-        <input type="radio" name="geschlecht" id="d" value="divers"><br><br>
+        <input type="radio" id="d"
+               name="geschlecht" value="divers"><br><br>
       </div>
       <div class="input-container">
         <span>Sprachen:</span><br>
         <label for="php">php</label>
-        <input type="checkbox" name="sprache[]" id="php" value="php">
+        <input type="checkbox"
+               name="sprache[]" id="php" value="php">
         <label for="py">python</label>
-        <input type="checkbox" name="sprache[]" id="py" value="python">
-        <label for="js">javascript</label>
-        <input type="checkbox" name="sprache[]" id="js" value="javascript"><br><br>
+        <input type="checkbox"
+               name="sprache[]" id="py" value="python">
+        <label for="js">javaScript</label>
+        <input type="checkbox" id="js"
+               name="sprache[]" value="javaScript"><br><br>
       </div>
       <div class="input-container">
         <label for="auto">Eiskugeln:</label><br>
-        <select class="tra-li o-off no-scroll mt0" name="auto[]" id="auto" multiple>
+        <select class="tra-li o-off no-scroll gl mt0"
+                name="auto[]" id="auto" multiple>
           <option value="1" selected>Vanille</option>
           <option value="2">Erdbeer</option>
           <option value="3" selected>Schoko</option>
           <option value="4">Zitrone</option>
-        </select><br><br>
+        </select><br>
       </div>
-      <div class="input-container">
+      <span class="hint">
+        Mehrfachauswahl mit Ctrl (&#8984;) + Click!
+      </span>
+      <div class="input-container mt1">
         <label for="comment">Kommentar:</label><br>
-        <textarea class="tra-li o-off mt0" name="comment" id="comment" cols="30" rows="8"></textarea><br>
-        <button class="btn gl rd0" type="submit">abschicken</button>
+        <textarea class="tra-li o-off mt0" id="comment"
+                  name="comment" cols="40" rows="6">
+        </textarea><br>
+        <div class="input-container">
+          <button class="btn gm rd0" type="submit">
+            abschicken
+          </button>
+        </div>
       </div>
     </form>
   </div>
--- bbq-arbeit/html/formInputTypesOutput.php
+++ bbq-arbeit/html/formInputTypesOutput.php
@@ -1,18 +1,13 @@
 <!doctype html>
 <html lang="de">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=">
-    <title>pampeldoc</title>
-    <style>
-        body {
-            background-color: #444;
-            color: #ddd;
-            font-size: 1.4rem;
-        }
-    </style>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=">
+  <link rel="stylesheet" href="../public/css/root.css">
+  <link rel="stylesheet" href="../public/css/style.css">
+  <title>pampeldoc</title>
 </head>
 <body>
 <?php
