75b7b848f0088242269eb157bf5cbc50f83c3e60
pytherik <erikberndt@gmx.net>
2023-05-17 22:29:23 +0200

    usw

--- javascript/kassenzettel/js/kassenzettel.js
+++ javascript/kassenzettel/js/kassenzettel.js
@@ -2,18 +2,21 @@ const container = document.querySelector('.container');
 const calc = document.querySelector('#calc');
 
 const head = ['Name', 'Anzahl', 'Einzelpreis', 'Total'];
-const ids = ['name', 'anz', 'price', 'total'];
+const idTags = ['name', 'anz', 'price', 'total'];
 
 for (let i = 0; i < 4; i++) {
   let ability = '';
   container.innerHTML += `<div id="col${i}" class="column">`;
-  document.getElementById(`col${i}`).innerHTML += `<span class="table-head">${head[i]}</span>`
+  document.getElementById(`col${i}`).innerHTML += `
+                                                  <div class="input-container">
+                                                    <span class="table-head">${head[i]}</span>
+                                                  </div>`
   if (i === 3) ability = 'disabled';
   for (let j = 0; j < 4; j++) {
     document.getElementById(`col${i}`).innerHTML += `
-                    <div class="input-container">
-                      <input id="${ids[i]}${j}"type="text" ${ability}/>
-                    </div>`;
+                                                    <div class="input-container">
+                                                      <input id="${idTags[i]}${j}"type="text" ${ability}/>
+                                                    </div>`;
   }
   container.innerHTML += '</div>';
 }
@@ -22,9 +25,9 @@ calc.addEventListener('click', () => {
   const totalsArr = [];
   let sum = 0;
   for (let i = 0; i < 4; i++) {
-    let anz = Number(document.getElementById(`${ids[1]}${i}`).value);
-    let price = Number(document.getElementById(`${ids[2]}${i}`).value);
-    document.getElementById(`${ids[3]}${i}`).value = `${anz * price}`;
+    let anz = Number(document.getElementById(`${idTags[1]}${i}`).value);
+    let price = Number(document.getElementById(`${idTags[2]}${i}`).value);
+    document.getElementById(`${idTags[3]}${i}`).value = `${anz * price}`;
     totalsArr.push(anz * price);
   }
   totalsArr.forEach((val) => {
