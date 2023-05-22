00648f09de4b7ab6e1a3d555cee906810d70973f
pytherik <erikberndt@gmx.net>
2023-05-22 19:09:08 +0200

    remove jsTutorial

--- XtraCode/jsTutorials/ajax/ajaxAdressen/ConnDB.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class ConnDB
-{
-  public static function connect():object
-  {
-    return new PDO(DB_DSN, DB_USER, DB_PASSWD);
-  }
-}
--- XtraCode/jsTutorials/ajax/ajaxAdressen/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-const DB_DSN = 'mysql:host=localhost;dbname=arrayGenerator';
-const DB_USER = 'erik';
-const DB_PASSWD = '321null';
